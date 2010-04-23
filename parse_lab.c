a8833e5bb8162651d0b66224da0fce7e2d2bed7f
Alex Manelis <amanelis@gmail.com>
2010-04-23 12:58:24 -0500

    include update

--- parse_lab.c
+++ parse_lab.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/fcntl.h>
 #include <sys/time.h>
-#include <unistd.h>
 
 #include <dnet.h>
 #include <pcap.h>
